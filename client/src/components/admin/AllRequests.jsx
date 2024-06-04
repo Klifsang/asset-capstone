36618fd334164f3f00d44a594bd74a779ddd97c1
Klif <sang.wicklife@student.moringaschool.com>
2024-06-04 16:32:07 +0300

    added rejection message to a request

--- client/src/components/admin/AllRequests.jsx
+++ client/src/components/admin/AllRequests.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import HttpClient from '../../HttpClient';
+import React, { useEffect, useState } from "react";
+import HttpClient from "../../HttpClient";
 
 const AllRequests = () => {
   const [requests, setRequests] = useState([]);
@@ -14,47 +14,61 @@ const AllRequests = () => {
     getRequests();
   }, []);
 
-  const approve = async (id,message) => {
+  const approve = async (id, message) => {
     const response = await HttpClient.patch("api/requests/update", {
       id: id,
       status: message,
     });
     getRequests();
   };
+
   return (
     <>
-    <table>
-      <thead>
-        <tr>
-          <th>Asset</th>
-          <th>Requested By</th>
-          <th>Quantity</th>
-          <th>Status</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {requests.map((request) => (
+      <table>
+        <thead>
           <tr>
-          <td data-label = "Asset">{request.assetname}</td>
-          <td data-label = "Requested By">{request.username}</td>
-          <td data-label = "Quantity">{request.quantity}</td>
-          <td data-label = "Status">
-            <span className={request.status}>{request.status}</span>
-          </td>
-          <td data-label = "Action">
-            <span>
-            <button className="shyButton" onClick={() => approve(request.id, "Approved")}>Approve</button>
-            <button className="shyButton" onClick={() => approve(request.id, "Rejected")}>Reject</button>
-            </span>
-          </td>
+            <th>Asset</th>
+            <th>Requested By</th>
+            <th>Quantity</th>
+            <th>Status</th>
+            <th>Action</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-    <blockquote> All Requests </blockquote>
-  </>
-  )
-}
+        </thead>
+        <tbody>
+          {requests.map((request) => (
+            <tr>
+              <td data-label="Asset">{request.assetname}</td>
+              <td data-label="Requested By">{request.username}</td>
+              <td data-label="Quantity">{request.quantity}</td>
+              <td data-label="Status">
+                <span className={request.status}>{request.status}</span>
+              </td>
+              <td data-label="Action">
+                <span>
+                  {request.status === "Approved" && (
+                    <button className="shyButton" onClick={() => approve(request.id, "Recalled")}>
+                      Recall
+                    </button>
+                  )}
+                  {request.status !== "Approved" && (
+                    <>
+                      <button className="shyButton" onClick={() => approve(request.id, "Approved")}>
+                        Approve
+                      </button>
+                      <button className="shyButton" onClick={() => approve(request.id, "Rejected")}>
+                        Reject
+                      </button>
+                    </>
+                  )}
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <blockquote> All Requests </blockquote>
+    </>
+  );
+};
 
-export default AllRequests
+export default AllRequests;

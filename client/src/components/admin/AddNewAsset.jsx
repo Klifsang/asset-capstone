af8ca2e1c21adbf8f1df45aa88f95e126854415d
Klif <sang.wicklife@student.moringaschool.com>
2024-06-04 22:11:20 +0300

    add image input box

--- client/src/components/admin/AddNewAsset.jsx
+++ client/src/components/admin/AddNewAsset.jsx
@@ -28,43 +28,51 @@ const AddNewAsset = () => {
       <form className="inputs" onSubmit={submitAsset}>
         <div className="flex one center">
           <div>
-            <div style="width: 200px">
-              <label class="dropimage">
+            <div style={{ width: 200 }}>
+              <label className="dropimage">
                 <input title="Drop image or click me" type="file" />
               </label>
             </div>
-            <label>Asset Name</label>
-            <input
-              type="text"
-              value={asset.assetname}
-              onChange={handleInputChange}
-              required
-              name="assetname"
-            />
-            <label>Quantity</label>
-            <input
-              type="number"
-              value={asset.quantity}
-              onChange={handleInputChange}
-              required
-              name="quantity"
-            />
-            <label>Condition</label>
-            <input
-              type="text"
-              value={asset.condition}
-              onChange={handleInputChange}
-              required
-              name="condition"
-            />
-            <label>Description</label>
-            <textarea
-              name="description"
-              onChange={handleInputChange}
-              id="description"
-              required
-              value={asset.description}
-            ></textarea>
+            <div>
+              <label>Asset Name</label>
+              <input
+                type="text"
+                value={asset.assetname}
+                onChange={handleInputChange}
+                required
+                name="assetname"
+              />
+            </div>
+            <div>
+              <label>Quantity</label>
+              <input
+                type="number"
+                value={asset.quantity}
+                onChange={handleInputChange}
+                required
+                name="quantity"
+              />
+            </div>
+            <div>
+              <label>Condition</label>
+              <input
+                type="text"
+                value={asset.condition}
+                onChange={handleInputChange}
+                required
+                name="condition"
+              />
+            </div>
+            <div>
+              <label>Description</label>
+              <textarea
+                name="description"
+                onChange={handleInputChange}
+                id="description"
+                required
+                value={asset.description}
+              ></textarea>
+            </div>
           </div>
         </div>
         <button type="submit">Submit Asset</button>
